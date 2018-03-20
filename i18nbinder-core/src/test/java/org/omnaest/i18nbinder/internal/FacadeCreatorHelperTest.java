7076092cbc1b7242f2c5e2f660bc282d1ade3d5b
Marco Herrn <marco@mherrn.de>
2018-03-20 18:45:08 +0100

    Generate Facade in temporary directory

--- i18nbinder-core/src/test/java/org/omnaest/i18nbinder/internal/FacadeCreatorHelperTest.java
+++ i18nbinder-core/src/test/java/org/omnaest/i18nbinder/internal/FacadeCreatorHelperTest.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2011 Danny Kunz
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,6 +17,7 @@ package org.omnaest.i18nbinder.internal;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.*;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
@@ -34,12 +35,12 @@ public class FacadeCreatorHelperTest
   private final static String[] PROPERTY_FILENAMES = { "adminTest_de_DE.properties", "adminTest_en_US.properties",
       "viewTest_de_DE.properties", "viewTest_en_US.properties", "localelessTest.properties", "673numericalTest_en_US.properties",
       "sub1/shared_de_DE.properties", "sub2/shared_de_DE.properties" };
-  
+
   /* ********************************************** Variables ********************************************** */
   private Set<File>             propertyFileSet    = new HashSet<File>();
-  
+
   /* ********************************************** Methods ********************************************** */
-  
+
   @Before
   public void setUp() throws Exception
   {
@@ -49,13 +50,13 @@ public class FacadeCreatorHelperTest
       this.propertyFileSet.add( new File( this.getClass().getResource( propertyFilename ).getFile() ) );
     }
   }
-  
+
   @Test
   public void testCreateI18nInterfaceFacadeFromPropertyFiles() throws IOException
   {
     //
     final String packageBaseFolder = "org" + File.separator + "omnaest" + File.separator + "i18nbinder" + File.separator + "internal";
-    
+
     //
     final String fileNameLocaleGroupPattern = null;
     final List<Integer> groupingPatternGroupingGroupIndexList = null;
@@ -65,7 +66,7 @@ public class FacadeCreatorHelperTest
     final String javaFacadeFileName = "I18nFacade";
     final boolean externalizeTypes = true;
     final String propertyFileEncoding = "utf-8";
-    
+
     Map<String, String> facadeFromPropertyFiles = FacadeCreatorHelper.createI18nInterfaceFacadeFromPropertyFiles( this.propertyFileSet,
                                                                                                                   new LocaleFilter(),
                                                                                                                   fileNameLocaleGroupPattern,
@@ -76,14 +77,17 @@ public class FacadeCreatorHelperTest
                                                                                                                   javaFacadeFileName,
                                                                                                                   externalizeTypes,
                                                                                                                   propertyFileEncoding );
-    
-    //    
+
+    //
     for ( String fileName : facadeFromPropertyFiles.keySet() )
     {
       //
-      final String basePath = new File( "" ).getAbsolutePath() + File.separator + "src" + File.separator + "test" + File.separator + "java" + File.separator + packageBaseFolder + File.separator + "facade" + File.separator;
+      final Path tmpPath= Files.createTempDirectory("i18nbuilder-core-FacadeCreatorHelperTest");
+      tmpPath.toFile().deleteOnExit();
+      final String basePath = tmpPath.toFile().getAbsolutePath() + File.separator + "src" + File.separator + "test" + File.separator + "java" + File.separator + packageBaseFolder + File.separator + "facade" + File.separator;
+      //final String basePath = new File( "" ).getAbsolutePath() + File.separator + "src" + File.separator + "test" + File.separator + "java" + File.separator + packageBaseFolder + File.separator + "facade" + File.separator;
       final String fileContent = facadeFromPropertyFiles.get( fileName );
-      
+
       //
       if ( fileName.contains( "." ) )
       {
@@ -102,10 +106,10 @@ public class FacadeCreatorHelperTest
         fileName = StringUtils.join( Arrays.copyOf( tokens, tokens.length - 1 ), "/" ) + ( tokens.length > 1 ? "/" : "" )
                    + tokens[tokens.length - 1];
       }
-      
+
       final File file = new File( basePath, fileName + ".java" );
       FileUtils.writeStringToFile( file, fileContent, "utf-8" );
     }
-    
+
   }
 }
